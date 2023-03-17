b79823fded2fd4b3c9322e4911967bebe40833cb
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-17 16:31:30 -0700

    print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-    int i;
+    int n;
 
-    for (i = 0; i < 10; i++)
+    for (n = 0; i < 10; n++)
 
-      putchar((i % 10) + '0');
+      putchar((n % 10) + '0');
 
 
     putchar('\n');
