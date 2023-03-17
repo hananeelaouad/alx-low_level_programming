3d35dc731b6a27f0cdd44f328d61f50f56ee4f48
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-17 15:29:57 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -17,11 +17,11 @@ int main(void)
     {
         if (c != 'e' && c != 'q')
         {
-            _putchar(c);
+            putchar(c);
         }
         c++;
     }
-    _putchar('\n');
+    putchar('\n');
 
     return (0);
 }
