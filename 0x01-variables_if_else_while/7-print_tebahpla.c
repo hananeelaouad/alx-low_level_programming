503d283f832476ed45dc0002bd54eca77e96baf5
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-17 16:47:16 -0700

    print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
-    char ch;
+	int letter;
 
-    for (ch = 'z'; ch >= 'a'; ch--)
-    {
-        putchar(ch);
-    }
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
-
