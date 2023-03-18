20c4090b9b6bc890f2eba08c4f33c42b9f90d070
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-17 17:49:26 -0700

    print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,32 +2,35 @@
 
 /**
  * main - Entry point
- *
+ *Description: print all possible different
+ *            combinations of two digits.
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
-    int i, j, k;
+	int digit1, digit2, digit3;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = i + 1; j < 9; j++)
-        {
-            for (k = j + 1; k < 10; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
-    return (0);
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
-
