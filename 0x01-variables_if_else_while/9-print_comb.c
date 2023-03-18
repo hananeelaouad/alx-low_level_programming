e72e4a9a8bbfa067f411dda8f02808abdbccf0e7
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-17 17:28:29 -0700

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,29 +2,24 @@
 
 /**
  * main - Entry point
- *Prints all possible combinations of single-digit numbers.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i, j;
+    int i;
 
     for (i = 0; i < 10; i++)
     {
-        for (j = i + 1; j < 10; j++)
+        putchar(i + '0');
+        if (i != 9)
         {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
+            putchar(',');
+            putchar(' ');
         }
     }
     putchar('\n');
 
     return (0);
 }
-
 
