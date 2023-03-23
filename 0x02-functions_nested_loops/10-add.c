36140a18666cad6bc816fc68bf8f515bf12b254f
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-22 19:27:27 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Suma dos enteros por función.
+ * @i: Primera varable entera a sumar.
+ * @l: Segunda variable a sumar.
+ * Return: retorno con sumatoria
+ */
+int add(int l, int i)
+{
+	int n;
+	 n = l + i;
+	return (n);
+}
