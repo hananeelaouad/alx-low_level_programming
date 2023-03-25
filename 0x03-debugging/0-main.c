eeddb2890235434e6acdc85175bf4e6f0e587fec
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-24 19:38:27 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *main - Debugging.
- *
- *Return: 0
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
 int main(void)
 {
@@ -12,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
+
+return (0);
 }
