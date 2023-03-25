13c5758fda5ac76b399c4445a200165ef03911c6
hannaeelaouad <hannaeelaouad9@gmail.com>
2023-03-24 18:31:23 -0700

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9, followed by a new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
